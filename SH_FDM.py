e294c2d60188841eed67eb975ac416fccde0488f
Omer80 <omertz@post.bgu.ac.il>
2015-01-19 11:34:04 +0200

    Modifying laplacian term

--- SH_FDM.py
+++ SH_FDM.py
@@ -9,25 +9,44 @@ __author__ = """Omer Tzuk (omertz@post.bgu.ac.il)"""
 
 import numpy as np
 from numpy.fft import fftn, ifftn, fftfreq
-import pylab
+from matplotlib import pyplot as plt
+
+
+def laplacian(u):
+    "This is the laplacian term"
+    hh = par['dx']**2
+    #print 'hh', hh
+    lap = (np.roll(u,1,axis=0) + np.roll(u,-1,axis=0) + np.roll(u,1,axis=1) + np.roll(u,-1,axis=1) -4.0*u)/hh
+    #print "laplacian", lap
+    return lap
+
+def laplaciansq(u):
+	"This is the laplacian^2 term"
+	dx2 = par['dx']**2
+	dx4 = dx**2
+	dy2 = par['dy']**2
+	dy4 = dy**2
+	u_xx = (np.roll(u,1,axis=1) + np.roll(u,-1,axis=1) -2.0*u)/dx2
+	u_xxyy = (np.roll(u_xx,1,axis=0) + np.roll(u_xx,-1,axis=0) -2.0*u_xx)/dy2
+	u_xxxx = (-4.0*np.roll(u,1,axis=1) -4.0*np.roll(u,-1,axis=1) +1.0*np.roll(u,2,axis=1) +1.0*np.roll(u,-2,axis=1) +6.0*u)/dx4
+	u_yyyy = (-4.0*np.roll(u,1,axis=0) -4.0*np.roll(u,-1,axis=0) +1.0*np.roll(u,2,axis=0) +1.0*np.roll(u,-2,axis=0) +6.0*u)/dy4
+	#print 'dx2:',dx2, ' dx4:',dx4,' dy4:',dy4
+	lap2 = (u_xxxx + 2.0*u_xxyy + u_yyyy)
+	#print "laplacian squared:", lap2
+	return lap2
 
 def local(u, dt, forcing, par):
     """This is the nonlinear function
        \lambda u^2 - u^3 + u * forcing   
     """
-    return u + dt * (par['Lambda']*u**2 - u**3 + u*forcing); # Local terms
-
-def laplacian(u):
-    "This is the laplacian term"
-    hh = par['dx']**2
-    return (np.roll(u,1,axis=0) + np.roll(u,-1,axis=0) + np.roll(u,1,axis=1) + np.roll(u,-1,axis=1) -4*u)/hh
+    return (dt * (par['Lambda']*u**2 - u**3 + u*forcing))
 
 def spatial(u):
 	"""
 	This is the spatial term of SH
-	- [ d^2/(dx)^2 + d^2/(dy)^2 + k0^2 ]^2 * u
+	 -[ d^2/(dx)^2 + d^2/(dy)^2 + k0^2 ]^2 * u
 	"""
-	return dt*(laplacian(u)-(par['k0'])**2)
+	return (dt * (-1.0)*(laplaciansq(u) + 2.0*laplacian(u)*(par['k0'])+(par['k0'])**2.0)) 
 
 
 par = {'epsilon':0.1,
@@ -45,8 +64,8 @@ par = {'epsilon':0.1,
 u0 = 0.5*(np.random.random((par['n'][0],par['n'][1]))-0.5)  # random initial conditions
 
 start  = 0.0
-step   = 5.0
-finish = 100.0
+step   = 1.0
+finish = 10.0
 
 
 # some extra calculations
@@ -61,7 +80,7 @@ if par['gamma'] != 0.0:
 dx = float(par['l'][0]) / par['n'][0]
 dy = float(par['l'][1]) / par['n'][1]
 
-dt = 0.00001/(2.0 * dx**2)
+dt = 0.0000001/(2.0 * dx**2)
 print "dt", dt
 
 par.update(dx=dx, dy=dy)
@@ -69,28 +88,32 @@ X,Y=np.mgrid[0:par['n'][0],0:par['n'][1]]
 X = X*dx
 Y = Y*dy
 forcing = par['gamma']*np.cos( par['kf']*X )
-step1 = u0.copy()            
+u_old = u0.copy()            
 
 # plot first frame (t=start)
-pylab.ion()
-pylab.clf()
+plt.ion()
+plt.clf()
 # u0.T is transpose because first index means lines (vertical), but we want it to represent x (horizontal)
 ext = [0,par['l'][0],0,par['l'][1]]
-im=pylab.imshow(u0.T,origin='lower', interpolation='nearest', extent=ext, cmap='Blues')
-cbar=pylab.colorbar()
-title=pylab.title('time=%2.1f'%start)
+im=plt.imshow(u0.T,origin='lower', interpolation='nearest', extent=ext, cmap='Blues')
+cbar=plt.colorbar()
+title=plt.title('time=%2.1f'%start)
 if par['gamma'] != 0.0: # if forcing, then draw red dashed lines
     for r in range(int(par['periods'])):
-        pylab.axvline(x=r*lambda_f,linewidth=1, color='red',linestyle='--')
-pylab.draw()
+        plt.axvline(x=r*lambda_f,linewidth=1, color='red',linestyle='--')
+        
+#plt.draw()
+plt.savefig('img_t_0.png', format='png', dpi=1000)
 
 t=start
 # start loop
 for tout in np.arange(start+step,finish+step,step):
     while t < tout:
-        step2 = local(step1, dt, forcing, par)
-        step1 = step2 + spatial(step1)
-        t+=dt
+		#print "time:", t
+		u_new = u_old + local(u_old, dt, forcing, par) + spatial(u_old)
+		u_old = u_new
+		t+=dt
     title.set_text('time=%.1f'%(t))
-    im.set_data((step1.real).T)
-    im.figure.canvas.draw()
+    im.set_data((u_new.real).T)
+    plt.savefig('img_t_%2.0f'%(t)+'.png', format='png', dpi=1000)
+    #im.figure.canvas.draw()
